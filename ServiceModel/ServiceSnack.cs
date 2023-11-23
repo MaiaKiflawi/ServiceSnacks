0779d1eb16ea67f9602edafdc07c2ed78d245424
MaiaKiflawi <MaiaKiflawi@gmail.com>
2023-11-23 10:31:04 +0200

    Add project files.

---
+++ ServiceModel/ServiceSnack.cs
@@ -0,0 +1,55 @@
+﻿using Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel;
+
+namespace ServiceModel
+{
+    public class ServiceSnack : IServiceSnack
+    {
+        public CityList GetAllCities()
+        {
+            CityDB db = new CityDB();
+            CityList list = db.SelectAll();
+            return list;
+        }
+
+        public SnackList GetAllSnacks()
+        {
+            SnackDB db = new SnackDB();
+            SnackList list = db.SelectAll();
+            return list;
+        }
+
+        public UserList GetAllUsers()
+        {
+            UserDB db = new UserDB();
+            UserList list = db.SelectAll();
+            return list;
+        }
+
+        public SnackList GetSnacksByUser(User user)
+        {
+            SnackDB db = new SnackDB();
+            SnackList list = db.SelectByUser(user);
+            return list;
+        }
+
+        public UserList GetUsersBySnack(Snack snack)
+        {
+            UserDB db = new UserDB();
+            UserList list = db.SelectBySnack(snack);
+            return list;
+        }
+
+        public User Login(User user)
+        {
+            UserDB db = new UserDB();
+            return db.LogIn(user);
+        }
+    }
+}
---
+++ ServiceSnacks/Snack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [DataContract]
+    public class Snack: BaseEntity
+    {
+        private string snackName;
+        private int calAmount;
+        public bool isSweet;
+        public int amount;
+        public int grams;
+        public bool like;
+        public string company;
+        [DataMember]
+        public string SnackName 
+        {
+            get { return snackName; }
+            set { snackName = value; }
+        }
+        [DataMember]
+        public int CalAmount
+        {
+            get { return calAmount; }
+            set { calAmount = value; }
+        }
+        [DataMember]
+        public bool IsSweet
+        {
+            get { return isSweet; }
+            set { isSweet = value; }
+        }
+        [DataMember]
+        public int Amount
+
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        [DataMember]
+        public int Grams
+        {
+            get { return grams; }
+            set { grams = value; }
+        }
+        [DataMember]
+        public bool Like
+        {
+            get { return like; }
+            set { like = value; }
+        }
+        [DataMember]
+        public string Company
+        {
+            get { return company; }
+            set { company = value; }
+        }
+    }
+    [CollectionDataContract]
+    public class SnackList : List<Snack>
+    {
+        public SnackList() { } //בנאי ברירת מחדל
+        public SnackList(IEnumerable<Snack> list) :
+            base(list)
+        { } //המרה של רשימת קורסים לאוסף של קורסים
+        public SnackList(IEnumerable<BaseEntity> list) :
+            base(list.Cast<Snack>().ToList())
+        { } //המרה כלפי מטה מישות בסיס לרשימת קורסים
+
+    }
+}
